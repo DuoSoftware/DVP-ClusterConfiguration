5637807f5c2fc0b1410a1a832e6bc159b1b7b4ab
sukitha <sukithaj@gmail.com>
2015-03-24 10:52:43 +0530

    config

---
+++ config/default.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "DB": {
+    "Type":"postgres",
+    "User":"duo",
+    "Password":"DuoS123",
+    "Port":5432,
+    "Host":"127.0.0.1",
+    "Database":"dvpdb"
+  },
+
+
+  "Redis":
+  {
+    "ip": "127.0.0.1",
+    "port": 6379
+
+  }
+};
