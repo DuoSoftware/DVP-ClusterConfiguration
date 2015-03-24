3bf90c3e0181f59662ac1200b190a6a0a69cdaa3
sukitha <sukithaj@gmail.com>
2015-03-24 16:23:00 +0530

    change config read

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var request = require('request');
 //var format = require('stringformat');
 
 var port = process.env.PORT || 3000;
-var host = config.host.domain || 'localhost';
+var host = config.Host.domain || 'localhost';
 
 //console.log(process.env);
 
