0b58c69de14a6838e769e1358ae9e7d9b5d4c48f
dinusha_dck <dinusha@duosoftware.com>
2017-08-01 17:58:44 +0530

    add config

--- RedisHandler.js
+++ RedisHandler.js
@@ -1,5 +1,5 @@
 var redis = require("ioredis");
-var Config = require('config');
+var config = require('config');
 var Redlock = require('redlock');
 var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
 var dbmodel = require('dvp-dbmodels');
